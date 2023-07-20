6f091713c7bfa89970d7aeb7ad58fc4ca1f2452e
Mariia Ovchinnikova <ghostlush@gmail.com>
2023-07-20 21:41:06 +0300

    Dice changes

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cubeImage2: UIImageView!
+    @IBOutlet weak var cubeImage1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        cubeImage1.image = UIImage(named: "DiceSix")
+        cubeImage2.image = UIImage(named: "DiceTwo")
+        
+ }
+    let diceArray = [UIImage(named: "DiceOne")!, UIImage(named: "DiceTwo")!, UIImage(named: "DiceThree")!, UIImage(named: "DiceFour")!, UIImage(named: "DiceFive")!, UIImage(named: "DiceSix")!]
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        cubeImage1.image = diceArray.randomElement()
+        cubeImage2.image = diceArray.randomElement()
+    
     }
-
-
+    
 }
 
